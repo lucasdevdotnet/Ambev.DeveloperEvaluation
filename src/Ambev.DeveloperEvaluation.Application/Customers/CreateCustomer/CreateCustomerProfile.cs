d84a3c9817fbf476b4d3eaa99439f1a2ae8b7afc
lucas <lucas.santana@pdcase.com.br>
2025-02-26 14:02:02 -0300

    - Create customer comand, profile and result

---
+++ src/Ambev.DeveloperEvaluation.Application/Customers/CreateCustomer/CreateCustomerProfile.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Application.Customers.CreateCustomer;
+using Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.Application.Sale.CreateSale;
+using Ambev.DeveloperEvaluation.Application.SalesItems.CreateSaleItem;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Customers.CreateCustomer;
+
+/// <summary>
+/// Profile for mapping between Customer entity and CreateCustomerResponse
+/// </summary>
+public class CreateCustomerProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateCustomer operation
+    /// </summary>
+    public CreateCustomerProfile()
+    {
+        CreateMap<CreateCustomerCommand, Customer>();
+        CreateMap<Customer, CreateCustomerResult>();
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Customers/CreateCustomer/CreateCustomerResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Customers.CreateCustomer;
+
+/// <summary>
+/// Represents the result of a customer creation operation.
+/// This class is used as a response object when a customer is successfully created.
+/// </summary>
+public class CreateCustomerResult
+{
+    /// <summary>
+    /// The name of the created customer.
+    /// Defaults to an empty string to prevent null values.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+}
