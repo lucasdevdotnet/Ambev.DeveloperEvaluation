8f871eac2b915eda2a8ceac41afb0bdc3b428724
lucas <lucas.santana@pdcase.com.br>
2025-02-26 14:23:35 -0300

    Create AutoMapper sales, salesItens, product and customer

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/CreateSaleItemRequestProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Application.SalesItems.CreateSaleItem;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSales;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class CreateSaleItemRequestProfile : Profile
+{
+    public CreateSaleItemRequestProfile()
+    {
+        CreateMap<CreateSaleItemRequest, CreateSaleItemCommand>();
+    }
+}
