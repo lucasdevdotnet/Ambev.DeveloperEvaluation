31f9a7d7afb0f9bcef199f4b4d57ff55fb2a7d37
lucas <lucas.santana@pdcase.com.br>
2025-02-26 14:04:05 -0300

    Create commad, profile and result

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+
+/// <summary>
+/// Represents a command to create a new product.
+/// This command is handled by a request handler to process the creation logic.
+/// </summary>
+public class CreateProductCommand : IRequest<CreateProductResult>
+{
+    /// <summary>
+    /// The name of the product to be created.
+    /// Defaults to an empty string to prevent null values.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The unit price of the product.
+    /// Defaults to 0M (decimal) to ensure a valid numeric value.
+    /// </summary>
+    public decimal UnitPrice { get; set; } = 0M;
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductProfile.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+
+/// <summary>
+/// Profile for mapping between SaleItem entity and CreateSaleItemResponse
+/// </summary>
+public class CreateProductProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateProduct operation
+    /// </summary>
+    public CreateProductProfile()
+    {
+        CreateMap<CreateProductCommand, Product>();
+
+    }
+}
