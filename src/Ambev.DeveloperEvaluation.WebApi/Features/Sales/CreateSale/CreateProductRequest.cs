8d49ef2607b0a51b44685cf831676f27ed997ae0
lucas <lucas.santana@pdcase.com.br>
2025-02-26 14:25:06 -0300

    Create feature sales

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateProductRequest.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSales;
+
+/// <summary>
+/// Represents a request to create a new product in the system.
+/// </summary>
+public class CreateProductRequest
+{
+    /// <summary>
+    /// The name of the product.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The unit price of the product.
+    /// </summary>
+    public decimal UnitPrice { get; set; } = 0;
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequestValidator.cs
@@ -0,0 +1,44 @@
+﻿using Ambev.DeveloperEvaluation.Application.SalesItems.CreateSaleItem;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+using FluentValidation;
+using static Ambev.DeveloperEvaluation.Application.Sales.CreateSale.CreateSaleCommandValidator;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSales;
+
+
+public class CreateSaleRequestValidator : AbstractValidator<CreateSaleRequest>
+{
+   
+    public CreateSaleRequestValidator()
+    {
+        RuleFor(sale => sale.TotalAmount).NotEmpty();
+        RuleFor(sale => sale.Items)
+            .NotNull().WithMessage("The sale must have at least one item.")
+            .Must(items => items != null && items.Any()).WithMessage("The sale must contain at least one item.");
+
+        // Valida cada item na lista
+        RuleForEach(sale => sale.Items).SetValidator(new CreateSaleItemRequestValidator());
+        RuleFor(sale => sale.SaleDate).NotEmpty();
+        RuleFor(sale => sale.Branch).NotEmpty();
+        RuleFor(sale => sale.Customer).NotEmpty();
+        RuleFor(sale => sale.Customer.Name).NotEmpty();
+    }
+
+    public class CreateSaleItemRequestValidator : AbstractValidator<CreateSaleItemRequest>
+    {
+        public CreateSaleItemRequestValidator()
+        {
+            RuleFor(item => item.Product).NotEmpty().NotNull();
+            RuleFor(item => item.Product.Name).NotEmpty().NotNull();
+            RuleFor(item => item.Product.UnitPrice).NotEmpty().NotNull();
+            RuleFor(item => item.Product.Name).NotEmpty().NotNull();
+
+            RuleFor(item => item.Quantity)
+                .GreaterThan(0).WithMessage("The quantity must be greater than 0.");
+
+            RuleFor(item => item.UnitPrice)
+                .GreaterThan(0).WithMessage("The unit price must be greater than 0.");
+        }
+    }
+}
