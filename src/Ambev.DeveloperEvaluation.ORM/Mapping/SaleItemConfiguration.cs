003b0e248b955875485850bb55a34ec26e165698
lucas <lucas.santana@pdcase.com.br>
2025-02-26 14:21:43 -0300

    Mapping tables to create sales

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+        builder.HasOne(si => si.Sale)
+           .WithMany(s => s.Items)
+           .HasForeignKey(si => si.SaleId);
+
+        builder.HasOne(si => si.Product)
+               .WithMany()
+               .HasForeignKey(si => si.ProductId);
+
+        builder.Property(si => si.UnitPrice).HasColumnType("decimal(18,2)");
+        builder.Property(si => si.Discount).HasColumnType("decimal(18,2)");
+        builder.Property(si => si.TotalPrice).HasColumnType("decimal(18,2)");
+    }
+}
